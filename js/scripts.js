c16823a0d61b2bfb214bde11dd7acd8c2af86b75
vakungur <vakungur@gmail.com>
2019-11-26 13:21:56 +0300

    Создание блока меню, а так же написание стилей и скриптов для него

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 // Бургер меню
 let hamburger = document.querySelector('.menu__hamburger');
-console.log(hamburger);
 
 if(hamburger) {
 
@@ -28,4 +27,21 @@ $('.carousel').owlCarousel({
             items:1
         }
     }
-})
+});
+
+// Модальное окно
+
+let btn = document.querySelector('.menu__button'),
+    modal = document.querySelector('.modal'),
+    filter = document.querySelector('.modal__filter')
+    close = document.querySelector('.modal__close');
+   
+    btn.addEventListener('click', function() {
+        modal.style.display = 'block';
+        filter.style.display = 'block';
+    });
+
+    close.addEventListener('click', function(){
+        modal.style.display = 'none';
+        filter.style.display = 'none';
+    })
